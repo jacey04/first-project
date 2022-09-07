e7744039913bfb5a5c72c0182c4be63ad7438789
jacey04 <113055266+jacey04@users.noreply.github.com>
2022-09-07 14:11:15 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("BYE")
+})
+basic.showIcon(IconNames.Heart)
